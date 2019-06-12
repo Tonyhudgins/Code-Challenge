5da98b5dcbf86b1e2ad4538f103bc080cd38d3f5
Tony Hudgins <tonyhudgins@gmail.com>
2019-06-12 16:11:06 -0700

    Basic font inclusion in Weather

--- app/components/Weather.js
+++ app/components/Weather.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react'
 import { Text, View, Image, StyleSheet } from 'react-native'
 import { Dimensions } from 'react-native';
+import { Font } from 'expo';
 
 export default class BookmarksPage extends Component {
+  constructor() {
+    super()
+    this.state = {
+      fontLoaded: false,
+    }
+  }
+
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      'SF-Pro-Display-Light': require('../assets/fonts/SF-Pro-Display-Light.otf'),
+      'SF-Pro-Display-Medium': require('../assets/fonts/SF-Pro-Display-Medium.otf'),
+    })
+    this.setState({fontLoaded: true})
+  }
   render() {
     return (
       <View style={styles.innerContainer}>
         <Image style={{width:width}} source={require('../assets/images/weatherHeader.png')}></Image>
-        <View style={styles.weatherText}>
-          <Text style={styles.text}> Good Morning </Text>
-          <Text style={styles.text}> Today is 72{'\u00b0'} and sunny </Text>
-        </View>
+        {this.state.fontLoaded ? 
+          (
+            <View style={styles.weatherText}>
+              <Text style={styles.salutation}>Good Morning </Text>
+              <Text style={styles.text}>Today is 72{'\u00b0'} and sunny </Text>
+            </View>
+          )
+        :
+          (null)
+        }
       </View>
     )
   }
@@ -30,7 +52,12 @@ const styles = StyleSheet.create({
     position: 'absolute',  
   },
 
+  salutation: {
+    fontFamily: 'SF-Pro-Display-Light', fontSize: 32,
+    marginVertical: 10, 
+  },
   text: {
+    fontFamily: 'SF-Pro-Display-Medium', fontSize: 13,
     marginVertical: 10, 
   },
 })
